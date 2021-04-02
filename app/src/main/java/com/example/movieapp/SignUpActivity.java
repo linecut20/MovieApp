8896b3515993478083fa5e17de1fff7fdc676bd4
2021itb <syw7777@naver.com>
2021-04-02 19:38:15 +0900

    activity_sign_up.xml, AndroidManifest.xml, build.gradle, SignUpActivity.java 수정 및 추가

--- app/src/main/java/com/example/movieapp/SignUpActivity.java
+++ app/src/main/java/com/example/movieapp/SignUpActivity.java
@@ -1,17 +1,23 @@
 package com.example.movieapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -24,11 +30,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.SimpleMultiPartRequest;
 import com.android.volley.toolbox.Volley;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
-import com.example.movieapp.request.RegisterRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -48,7 +56,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText edtID,edtPassword,edtName, edtImage, edtPhone, edtEmail, edtBirth, edtGender;
+    private CircleImageView circleImageView;
     private Button btnRegister, btnBack;
+
+    private String imgPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,73 +67,154 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         findFunc();
-        eventFunc();
-    }
-
-    private void eventFunc() {
-
-
-
-
-        btnRegister.setOnClickListener(v->{
-            String ID = edtID.getText().toString();
-            String password = edtPassword.getText().toString();
-            String name = edtName.getText().toString();
-//            String image = "이미지 파일 임시 주소";
-//            String image = edtImage.getText().toString();
-            String phone = edtPhone.getText().toString();
-            String email = edtEmail.getText().toString();
-            String birth = edtBirth.getText().toString();
-            String gender = edtGender.getText().toString();
-
-
-            Response.Listener<String> listener = new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject jo = new JSONObject(response);
-                        boolean success = jo.getBoolean("success");
-                        if(success==true){
-                            toastMessage(name + "님 회원가입 성공");
-                            Intent intent = new Intent(SignUpActivity.this,ProfileActivity.class);
-                            startActivity(intent);
-                        }else{
-                            toastMessage("회원 가입 실패");
-                            return;
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-//            RegisterRequest registerRequest = new RegisterRequest(ID, password, name, image, phone, email, birth, gender, listener);
-            RegisterRequest registerRequest = new RegisterRequest(ID, password, name, phone, email, birth, gender, listener);
-            RequestQueue queue = Volley.newRequestQueue(SignUpActivity.this);
-            queue.add(registerRequest);
-        });
-
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 finish();
             }
         });
+
+        //외부 저장소에 권한 필요, 동적 퍼미션
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            int permissionResult= checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if(permissionResult== PackageManager.PERMISSION_DENIED){
+                String[] permissions= new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,10);
+            }
+        }
     }
 
-    private void toastMessage(String s) {
-        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case 10:
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "외부 메모리 읽기/쓰기 사용 가능", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(this, "외부 메모리 읽기/쓰기 제한", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
+
+    public void clickBtn(View view) {
+        Intent intent= new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,10);
+    }
+
+    public void clickIv(View view) {
+        Intent intent= new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 10:
+                if(resultCode==RESULT_OK){
+                    Toast.makeText(this, "RESULT_OK", Toast.LENGTH_SHORT).show();
+                    Uri uri= data.getData();
+                    if(uri!=null){
+                        circleImageView.setImageURI(uri);
+                        //갤러리앱에서 관리하는 DB정보가 있는데, 그것이 나온다 [실제 파일 경로가 아님!!]
+                        //얻어온 Uri는 Gallery앱의 DB번호임. (content://-----/2854)
+                        //업로드를 하려면 이미지의 절대경로(실제 경로: file:// -------/aaa.png 이런식)가 필요함
+                        //Uri -->절대경로(String)로 변환
+                        imgPath= getRealPathFromUri(uri);   //임의로 만든 메소드 (절대경로를 가져오는 메소드)
+
+                        //이미지 경로 uri 확인해보기
+                        new AlertDialog.Builder(this).setMessage(uri.toString()+"\n"+imgPath).create().show();
+                    }
+                }else{
+                    Toast.makeText(this, "이미지 선택을 하지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }// onActivityResult()..
+
+    //Uri -- > 절대경로로 바꿔서 리턴시켜주는 메소드
+    String getRealPathFromUri(Uri uri){
+        String[] proj= {MediaStore.Images.Media.DATA};
+        CursorLoader loader= new CursorLoader(this, uri, proj, null, null, null);
+        Cursor cursor= loader.loadInBackground();
+        int column_index= cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result= cursor.getString(column_index);
+        cursor.close();
+        return  result;
+    }
+
+    public void clickUpload(View view) {
+        //서버로 보낼 데이터
+        String ID = edtID.getText().toString();
+        String password = edtPassword.getText().toString();
+        String name = edtName.getText().toString();
+//            String image = "이미지 파일 임시 주소";
+//            String image = edtImage.getText().toString();
+        String phone = edtPhone.getText().toString();
+        String email = edtEmail.getText().toString();
+        String birth = edtBirth.getText().toString();
+        String gender = edtGender.getText().toString();
+
+
+        //안드로이드에서 보낼 데이터를 받을 php 서버 주소
+        String serverUrl="http://itb2021.dothome.co.kr/insertDBUserTBL.php";
+
+        //Volley plus Library를 이용해서
+        //파일 전송하도록..
+        //Volley+는 AndroidStudio에서 검색이 안됨 [google 검색 이용]
+
+        //파일 전송 요청 객체 생성[결과를 String으로 받음]
+        SimpleMultiPartRequest smpr= new SimpleMultiPartRequest(Request.Method.POST, serverUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                new AlertDialog.Builder(SignUpActivity.this).setMessage("응답:"+response).create().show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SignUpActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //요청 객체에 보낼 데이터를 추가
+        smpr.addStringParam("ID", ID);
+        smpr.addStringParam("password", password);
+        smpr.addStringParam("name", name);
+        smpr.addStringParam("phone", phone);
+        smpr.addStringParam("email", email);
+        smpr.addStringParam("birth", birth);
+        smpr.addStringParam("gender", gender);
+
+        //이미지 파일 추가
+        smpr.addFile("img", imgPath);
+
+        //요청객체를 서버로 보낼 우체통 같은 객체 생성
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(smpr);
+
+    }
+
+    public void clickLoad(View view) {
+    }
+
+
 
     private void findFunc() {
         edtID = findViewById(R.id.edtID);
         edtPassword = findViewById(R.id.edtPassword);
         edtName = findViewById(R.id.edtName);
-//        edtImage= findViewById(R.id.edtImage);
+
         edtPhone= findViewById(R.id.edtPhone);
         edtEmail= findViewById(R.id.edtEmail);
         edtBirth= findViewById(R.id.edtBirth);
         edtGender= findViewById(R.id.edtGender);
+
+        circleImageView=findViewById(R.id.circleImageView);
 
         btnRegister = findViewById(R.id.btnRegister);
         btnBack = findViewById(R.id.btnBack);
